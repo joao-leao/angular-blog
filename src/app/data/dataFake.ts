42a022b6992f383468c37f6f9d7ad640b1153f80
joao-leao <flocoswhite@gmail.com>
2024-02-09 21:21:29 -0300

    :sparkles: feat: content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam sequi optio eum vero excepturi necessitatibus soluta itaque repellendus, nihil atque distinctio repellat doloremque accusamus. Porro illo in rerum eveniet consectetur?",
+        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=webp"
+    },
+    {
+        "id":"2",
+        "title": "Nova Série anunciada no Disney +",
+        "description": "BLA BLA BLA",
+        "photoCover": "https://tm.ibxk.com.br/2022/05/04/04181551056727.jpg?ims=1200x675"
+    }
+]
