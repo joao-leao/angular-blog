fa0739170fca4f789429e99e4581d17a70f1027f
joao-leao <flocoswhite@gmail.com>
2024-02-13 18:29:42 -0300

    :wrench: change in data

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,7 +8,31 @@ export const dataFake = [
     {
         "id":"2",
         "title": "Nova Série anunciada no Disney +",
-        "description": "BLA BLA BLA",
+        "description": `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
         "photoCover": "https://tm.ibxk.com.br/2022/05/04/04181551056727.jpg?ims=1200x675"
+    },
+    {
+        "id":"3",
+        "title": "Jonathan Majors é demitido pela Marvel após ser considerado culpado por agressão e assédio",
+        "description": `O ator americano Jonathan Majors foi considerado culpado nesta segunda-feira (18) pelas acusações de agressão e assédio movidas pela ex-namorada, Grace Jabbari, em março deste ano. 
+
+        As informações foram reveladas pela Variety, que menciona ainda que ele foi inocentado de outras duas acusações, que citavam agressão intencional de terceiro grau e assédio agravado. A revista também informou que a sentença será divulgada no dia 6 de fevereiro pelo juiz Michael Gaffey. Ele pode pegar até um ano de prisão.
+        
+        Nova série sobre outros personagens de "A Família Addams" pode ser lançada, diz siteNova série sobre outros personagens de "A Família Addams" pode ser lançada, diz site
+        Carol Castro anuncia morte do pai, o ator Luca de Castro, e recebe apoio de famososCarol Castro anuncia morte do pai, o ator Luca de Castro, e recebe apoio de famosos
+        Após o veredito, a Marvel anunciou a demissão do ator. A informação foi confirmada pelo Deadline. Jonathan Majors ficou conhecido pelo papel do vilão Kang, O Conquistador em Homem Formiga e a Vespa: Quantumania (2023), e já estava cotado para estrelar um filme solo do personagem. Agora, outro ator deverá ser escalado para o papel.  
+        
+        O artista, que também ficou conhecido pelo trabalho em Creed 3 (2023), foi preso no dia 25 de março, depois que a ex-namorada o denunciou por agressão, estrangulamento e assédio.
+        
+        Segundo o Deadline, a vítima relatou à polícia que o ator a agrediu depois de uma briga. Na ocasião, Grace estava com marcas no rosto e tinha uma laceração atrás da orelha. Advogados do ator alegam que a ex-companheira de Majors está mentindo.`,
+        "photoCover": "https://www.rbsdirect.com.br/filestore/8/2/6/3/8/8/4_05485adf959862b/4883628_9b10c6908cb3cc6.jpg?w=700"
+    },
+    {
+        "id":"4",
+        "title": `RBS TV exibe "Vingadores: Ultimato" neste fim de semana`,
+        "description": `A Temperatura Máxima deste domingo (24), na RBS TV, vai exibir o maior sucesso comercial entre os filmes de super-herói: Vingadores: Ultimato (2019), dono da segunda maior bilheteria de todos os tempos, com US$ 2,79 bilhões arrecadados. É a aventura que fechou uma grande história que a Marvel vinha contando havia mais de 10 anos, desde que Homem de Ferro (208) dera início a seu universo cinematográfico.`,
+        "photoCover": "https://lumiere-a.akamaihd.net/v1/images/infinitywarthanos_70feb51d.jpeg?region=63,0,1899,1068&width=960"
     }
 ]
